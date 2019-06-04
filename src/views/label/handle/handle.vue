c7186d8371e363e13ff6b8f7e53a859cab313069
itw_liuxn <itw_liuxn@tkmail.com>
2019-06-04 17:50:22 +0800

    handle~~~

--- src/views/label/handle/handle.vue
+++ src/views/label/handle/handle.vue
@@ -1,13 +1,53 @@
 <template>
   <div>
-   手工生成质检
+    <a-date-picker @change="onChange" />
+    <a-button type="primary"  @click="generate()" :loading="loading">生成质检任务</a-button>
   </div>
 
 </template>
 
 <script>
 export default {
-  name: 'handle'
+  name: 'handle',
+  data () {
+    return {
+      loading: false,
+      date: ''
+    }
+  },
+  methods: {
+    onChange (date, dateString) {
+      console.log(date, dateString)
+      this.date = dateString
+    },
+    generate () {
+      if (this.date == '') {
+        this.$warning({
+          title: '日期不能为空',
+          content: '请选择日期',
+          maskClosable: true
+        })
+        return
+      }
+      this.loading = true
+      let params = {
+        type: 'POST',
+        url: this.baseUrl + '/generate_quality_check',
+        data: {
+          'date': this.date
+        },
+        success: (res) => {
+          console.log(res)
+          this.loading = false
+        },
+        error: (err) => {
+          console.log('error!', err)
+          this.loading = false
+        }
+      }
+      this.sendAjax(params)
+    }
+  }
 }
 </script>
 
