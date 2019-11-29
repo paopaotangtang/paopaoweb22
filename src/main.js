8c87c91cb8146e60abec648c8e8b00170c83af9f
meto001 <756246975@qq.com>
2019-11-29 10:03:42 +0800

    test2

--- src/main.js
+++ src/main.js
@@ -13,13 +13,15 @@ Vue.use(antd)
 
 Vue.config.productionTip = false
 // Vue.prototype.baseUrl = 'https://www.easy-mock.com/mock/5c931ac12418a911d0e22aa7/paopaoweb'//这里是基础路由，以后铺设服务器也是改这个！
-// Vue.prototype.baseUrl = 'http://localhost:82'
-Vue.prototype.baseUrl = 'http://182.61.51.47:82'
+Vue.prototype.baseUrl = 'http://localhost:8282'
+// Vue.prototype.baseUrl = 'http://192.168.0.196:8282'
+// Vue.prototype.baseUrl = 'http://182.61.51.47:82'
 Vue.prototype.sendAjax = (params) => {
   $.ajax({
     type: params.type || 'get',
     url: params.url,
     dataType: 'json',
+    timeout: params.timeout || -1,
     contentType: 'application/json',
     async: params.async || true,
     data: (params.type == 'post' || params.type == 'POST') ? JSON.stringify(params.data) : params.data,
